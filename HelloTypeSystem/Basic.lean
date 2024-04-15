f2041dd16ec358ffd4f5f6c65870f633366c623c
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-04-15 22:06:28 +0000

    put definitions in order

--- HelloTypeSystem/Basic.lean
+++ HelloTypeSystem/Basic.lean
@@ -5,42 +5,27 @@ inductive PNat
   | Z
   | S (n : PNat)
 
-def PNat.toNat : PNat → Nat
-  | .Z   => Nat.zero
-  | .S n => Nat.succ n.toNat
-
-theorem PNat.toNat_succ (n : PNat) : 1 + PNat.toNat n = PNat.toNat (.S n) :=
-  calc 1 + PNat.toNat n
-    _ = PNat.toNat n + 1 := Nat.add_comm _ _
-
 instance : OfNat PNat 0 where
   ofNat := PNat.Z
 
 instance (n : Nat) [OfNat PNat n] : OfNat PNat (Nat.succ n) where
   ofNat := PNat.S (OfNat.ofNat n)
 
-instance : ToString PNat where
-  toString n := s!"{n.toNat}"
+def PNat.toNat : PNat → Nat
+  | .Z   => Nat.zero
+  | .S n => Nat.succ n.toNat
 
 instance : Coe PNat Nat where
   coe n := n.toNat
 
-def PNat.add : PNat → PNat → PNat
-  | .Z, k => k
-  | .S n, k => .S (PNat.add n k)
-
-instance : Add PNat where
-  add := PNat.add
-
-def PNat.mul : PNat → PNat → PNat
-  | .Z,   _ => .Z
-  | .S n, k => k + PNat.mul n k
-
-instance : Mul PNat where
-  mul := PNat.mul
+instance : ToString PNat where
+  toString n := s!"{n.toNat}"
 
 /--
 判断
+
+この型の項は形式上は正しい判断であるが、内容的にも正しいとは限らない。
+内容的な正しい判断は導出システムによって規定される。
 -/
 inductive Judgement where
   /--
