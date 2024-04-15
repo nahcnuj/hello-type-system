1adb0a79945362bcb712a52eb0963ae433e853c5
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-04-15 22:47:51 +0000

    fix

--- HelloTypeSystem.lean
+++ HelloTypeSystem.lean
@@ -8,11 +8,13 @@ import «HelloTypeSystem».Derivation
 Lean 4でプログラミング言語の型システムや意味論に入門するリポジトリです。
 ## Index
 - [諸定義](./HelloTypeSystem/Basic.html)
-  - [ペアノ自然数`PNat`](./HelloTypeSystem/Basic.html#PNat)
-  - [判断(judgement)](./HelloTypeSystem/Basic.html#Judgement)
+  - [ペアノ自然数`PNat`](./HelloTypeSystem/Basic.html#HelloTypeSystem.PNat)
+  - [判断(judgement)](./HelloTypeSystem/Basic.html#HelloTypeSystem.Judgement)
 - 導出システム(derivation systems)
-  - [`Nat`](./HelloTypeSystem/Derivation/Nat.html)
-  - [`CompareNat1`](./HelloTypeSystem/Derivation/CompareNat.html##CompareNat1.Derivation)
+  - [`Nat`](./HelloTypeSystem/Derivation/Nat.html#HelloTypeSystem.Derivation.Nat)
+  - [`CompareNat1`](./HelloTypeSystem/Derivation/CompareNat.html#HelloTypeSystem.CompareNat1.Derivation)
+  - [`CompareNat2`](./HelloTypeSystem/Derivation/CompareNat.html#HelloTypeSystem.CompareNat2.Derivation)
+  - [`CompareNat3`](./HelloTypeSystem/Derivation/CompareNat.html#HelloTypeSystem.CompareNat3.Derivation)
 ## References
 - 五十嵐淳 著. プログラミング言語の基礎概念, サイエンス社, 2011.7, (ライブラリ情報学コア・テキスト ; 24). 978-4-7819-1285-1. [https://ndlsearch.ndl.go.jp/books/R100000002-I000011238587](https://ndlsearch.ndl.go.jp/books/R100000002-I000011238587)
 - 大堀淳 著. プログラミング言語の基礎理論. 新装版, 共立出版, 2019.8. 978-4-320-12450-9. [https://ndlsearch.ndl.go.jp/books/R100000002-I029842615](https://ndlsearch.ndl.go.jp/books/R100000002-I029842615)
