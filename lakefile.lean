69a1291a02500c11eeffdddf7eed34c0eeb8f9e0
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-07-01 16:15:37 +0000

    update Lean to 4.9.0

--- lakefile.lean
+++ lakefile.lean
@@ -12,4 +12,4 @@ lean_exe «hellotypesystem» where
   root := `Main
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "c7f4ac84b973b6efd8f24ba2b006cad1b32c9c53" -- "main"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "1daa0de7eea6bf8d3e1b0814156ede21a8a9e9e9" -- "main"
